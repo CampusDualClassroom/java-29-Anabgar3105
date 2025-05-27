85a2b7e9308a5c1f7f7b83c8fa20aab7613fb8a0
Ana Belén García Añón <agarciaanon@gmail.com>
2025-05-27 12:41:28 +0200

    Exercise29 solved.

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,19 @@
 package com.campusdual.classroom;
 
 public class Exercise29 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        try {
+            System.out.println(posArray(arr, 3));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("La última posición del array es: " + (arr.length - 1));
+        }
+    }
+
+    public static int posArray(int[] array, int pos) throws ArrayIndexOutOfBoundsException {
+        return array[pos];
     }
 }
